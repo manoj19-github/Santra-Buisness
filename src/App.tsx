a399afeee2252074ccf02fc277545adc054d32f2
manoj-fleapo <106957653+manoj-fleapo@users.noreply.github.com>
2023-01-19 18:10:23 +0530

    sidebar working progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { lazy, Suspense } from "react";
+import { ColorModeContext, tokens, useMode } from "./theme";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./pages/dashboard/global/Topbar";
+import Sidebar from "./pages/dashboard/global/Sidebar";
+import { Routes, Route } from "react-router-dom";
+// import Team from "./pages/team";
+// import Invoices from "./pages/invoices";
+// import Contacts from "./pages/contacts";
+// import Form from "./pages/Form";
+// import Bar from "./pages/bar";
+// import Bar from "./pages/bar";
+// import Bar from "./pages/bar";
 
 function App() {
+  const [theme, colorMode] = useMode();
+  const Team = lazy(() => import("./pages/team"));
+  const Invoices = lazy(() => import("./pages/invoices"));
+  const Line = lazy(() => import("./pages/line"));
+  const Contacts = lazy(() => import("./pages/contacts"));
+  const Bar = lazy(() => import("./pages/bar"));
+  const Pie = lazy(() => import("./pages/invoices/pie"));
+  const Form = lazy(() => import("./pages/form"));
+  const FAQ = lazy(() => import("./pages/faq"));
+  const Geography = lazy(() => import("./pages/geography"));
+  const Dashboard = lazy(() => import("./pages/dashboard"));
+  const Calendar = lazy(() => import("./pages/calendar"));
+
   return (
-    <div className="app">
-      
-    </div>
+    <ColorModeContext.Provider value={colorMode as any}>
+      <ThemeProvider theme={theme as any}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+            <Suspense>
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/team" element={<Team />} />
+                <Route path="/invoices" element={<Invoices />} />
+                <Route path="/line" element={<Line />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/form" element={<Form />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/line" element={<Line />} />
+                <Route path="/faq" element={<FAQ />} />
+                <Route path="/geography" element={<Geography />} />
+                <Route path="/calendar" element={<Calendar />} />
+              </Routes>
+            </Suspense>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
--- src/pages/dashboard/global/Sidebar.tsx
+++ src/pages/dashboard/global/Sidebar.tsx
@@ -1,7 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import "react-pro-sidebar/dist/css/styles.css";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+import { tokens } from "../../../theme";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlinedIcon from "@mui/icons-material/PieChartOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 
+const Item = ({
+  title,
+  to,
+  icon,
+  selected,
+  setSelected,
+}: {
+  title: string;
+  to: string;
+  icon: React.ReactNode;
+  selected: string;
+  setSelected: any;
+}) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <MenuItem
+      active={selected === title}
+      style={{
+        color: colors.grey[100],
+      }}
+      onClick={() => setSelected(title)}
+      icon={icon}
+    >
+      <Typography>{title}</Typography>
+      <Link to={to} />
+    </MenuItem>
+  );
+};
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [isCollapsed, setIsColapsed] = useState<boolean>(false);
+  const [selected, setSelected] = useState<string>("Dashboard");
+  return (
+    <Box
+      sx={{
+        "& .pro-sidebar-inner": {
+          background: `${colors.primary[400]} !important`,
+        },
+        "& .pro-icon-wrapper": {
+          backgroundColor: `transparent !important`,
+        },
+        "& .pro-inner-item": {
+          padding: "5px 35px 20px !important",
+        },
+        "& .pro-inner-item:hover": {
+          color: "#868dfb !important",
+        },
+        "& .pro-menu-item.active": {
+          color: "#6870fa !important",
+        },
+      }}
+    >
+      <ProSidebar collapsed={isCollapsed}>
+        <Menu iconShape="square">
+          <MenuItem
+            onClick={() => setIsColapsed((prev) => !prev)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+              color: colors.grey[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+              >
+                <Typography variant="h3" color={colors.grey[100]}>
+                  Santra Buisness
+                </Typography>
+                <IconButton onClick={() => setIsColapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+        </Menu>
+      </ProSidebar>
+    </Box>
+  );
 };
 
 export default Sidebar;
